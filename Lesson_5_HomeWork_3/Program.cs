02be4eca52cdf0b3f5c4091526697bd95d4aff37
Kostya <ashirokovkonstantin@gmail.com>
2023-06-26 19:46:01 +0500

    Create lesson 5 homework 3

---
+++ Lesson_5_HomeWork_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson_5_HomeWork_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mas = new double[100];
+            Random x = new Random();
+            int i = 0;
+            while (i < mas.Length)
+            {
+                mas[i] = Convert.ToDouble(x.Next(10, 100)/10.0);
+                i++;
+            }
+
+            double max = mas[0];
+            double min = mas[0];
+            i = 0;
+
+            while (i < mas.Length)
+            {
+                if (mas[i] > max) max = mas[i];
+                if (mas[i] < min) min = mas[i];
+                i++;
+            }
+
+            Console.WriteLine("Разница между максимальным и минимальным числами массива равна " + (max-min));
+           
+        }
+    }
+}
