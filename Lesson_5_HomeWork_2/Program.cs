dc5ef3742af43a701acda61ae3dd2e170bfd4e62
Kostya <ashirokovkonstantin@gmail.com>
2023-06-26 19:32:35 +0500

    Create lesson 5 homework 2

---
+++ Lesson_5_HomeWork_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson_5_HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[100];
+            int i = 0;
+            int sum = 0;
+            while (i < mas.Length)
+            {
+                mas[i] = new Random().Next(-100, 101);
+                if (i%2 != 0) sum = sum + mas[i];
+                i++;
+            }
+            Console.WriteLine("Сумма элементов, стоящих на нечетных позициях равна " + sum);
+        }
+    }
+}
